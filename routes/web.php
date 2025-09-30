4e6ffb212f10250abc67f5d7c4ee108f42b6fe90
dophu1717 <dophu17@gmail.com>
2025-09-30 11:13:27 +0700

    layout frontend

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 
 Route::middleware('setLocale')->group(function () {
     Route::get('/', function () {
-        return redirect()->route('incidents.index');
+        return view('welcome');
     });
 
     // Auth routes
@@ -26,6 +26,11 @@ Route::middleware('setLocale')->group(function () {
     // Public feed
     Route::get('/incidents', [IncidentController::class, 'index'])->name('incidents.index');
 
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return redirect()->route('incidents.index');
+    })->name('dashboard');
+
     // Employee actions
     Route::middleware('auth')->group(function () {
         Route::get('/incidents/create', [IncidentController::class, 'create'])->name('incidents.create');
@@ -40,29 +45,4 @@ Route::middleware('setLocale')->group(function () {
     Route::middleware(['auth', 'role:manager'])->group(function () {
         Route::get('/admin/incidents', [IncidentController::class, 'index'])->name('admin.incidents.index');
     });
-});
-
-// Auth routes
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.post');
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register'])->name('register.post');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// Public feed
-Route::get('/incidents', [IncidentController::class, 'index'])->name('incidents.index');
-
-// Employee actions
-Route::middleware('auth')->group(function () {
-    Route::get('/incidents/create', [IncidentController::class, 'create'])->name('incidents.create');
-    Route::post('/incidents', [IncidentController::class, 'store'])->name('incidents.store');
-    Route::get('/incidents/{incident}', [IncidentController::class, 'show'])->name('incidents.show');
-    Route::get('/incidents/{incident}/edit', [IncidentController::class, 'edit'])->name('incidents.edit');
-    Route::put('/incidents/{incident}', [IncidentController::class, 'update'])->name('incidents.update');
-    Route::delete('/incidents/{incident}', [IncidentController::class, 'destroy'])->name('incidents.destroy');
-});
-
-// Admin area
-Route::middleware(['auth', 'role:manager'])->group(function () {
-    Route::get('/admin/incidents', [IncidentController::class, 'index'])->name('admin.incidents.index');
 });
